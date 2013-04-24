4ee4d2ae235ff8ef4d7e852710495fb8a92c3bda
George Rider <gmrider@gmail.com>
2013-04-24 00:58:49 -0700

    formatted for funsies

--- stocksie_personal.rb
+++ stocksie_personal.rb
@@ -6,46 +6,46 @@ require 'optparse'
 require 'yaml'
 
 def load_or_config(config)
-	if File.exist?("./stocksie_data.yaml")
-	  YAML.load(File.open("./stocksie_data.yaml","r"))
-	else
-	  config.config
-	end	
+  if File.exist?("./stocksie_data.yaml")
+    YAML.load(File.open("./stocksie_data.yaml","r"))
+  else
+    config.config
+  end 
 end
 
-# need to refactor options output. current string chaining is ugly.	
+# need to refactor options output. current string chaining is ugly. 
 # learn to iterate through a hash of nested arrays
-def optparse(monies,config)	
-	OptionParser.new(banner = nil, width = 15, indent =' ' * 4)	do |opts|  
-		opts.banner = "Usage:  #{$0} [-option]"
-		opts.on('-c', '--config', 'configure evaluation data for script')  	{ config.config }
-    opts.on('-m', 'shares vesting per month')                         	{ puts config.indent + monies.monthly.to_s }
+def optparse(monies,config) 
+  OptionParser.new(banner = nil, width = 15, indent =' ' * 4) do |opts|  
+    opts.banner = "Usage:  #{$0} [-option]"
+    opts.on('-c', '--config', 'configure evaluation data for script')   { config.config }
+    opts.on('-m', 'shares vesting per month')                           { puts config.indent + monies.monthly.to_s }
     opts.on('-M', 'dollar amount vesting per month')                    { puts config.indent + "$" + monies.value_monthly.to_s }  
-    opts.on('-t', 'months vested')                         							{ puts config.indent + monies.grant.months_at_company.to_s }
-    opts.on('-v','total shares vested')                         				{ puts config.indent + monies.vest_total.to_s }
+    opts.on('-t', 'months vested')                                      { puts config.indent + monies.grant.months_at_company.to_s }
+    opts.on('-v','total shares vested')                                 { puts config.indent + monies.vest_total.to_s }
     opts.on('-V', 'total dollar amount vested')                         { puts config.indent + "$" + monies.value_vested.to_s }
-    opts.on('-r', 'unvested shares')                         						{ puts config.indent + monies.remaining.to_s }
-    opts.on('-R', 'unvested dollar amount')                         		{ puts config.indent + "$" + monies.value_remain.to_s }
-    opts.on('-g', 'total options granted')                         			{ puts config.indent + monies.grant.grant_total.to_s }
+    opts.on('-r', 'unvested shares')                                    { puts config.indent + monies.remaining.to_s }
+    opts.on('-R', 'unvested dollar amount')                             { puts config.indent + "$" + monies.value_remain.to_s }
+    opts.on('-g', 'total options granted')                              { puts config.indent + monies.grant.grant_total.to_s }
     opts.on('-G', 'total value of options granted')                     { puts config.indent + "$" + monies.value_total.to_s }
-		opts.help
-		opts.on_head('Options:')
-	end
+    opts.help
+    opts.on_head('Options:')
+  end
 end
 
 def parse_option(optparse)
-	if ARGV.empty?
-		puts optparse.banner
-	elsif ARGV[0] == "-" || ARGV[0].match(/^[a-zA-Z]/)
-		puts optparse
-	else
-		begin optparse.parse!
+  if ARGV.empty?
+    puts optparse.banner
+  elsif ARGV[0] == "-" || ARGV[0].match(/^[a-zA-Z]/)
+    puts optparse
+  else
+    begin optparse.parse!
     rescue OptionParser::InvalidOption => e
       puts e
-			puts optparse
+      puts optparse
       exit 1
     end
-	end
+  end
 end
 
 

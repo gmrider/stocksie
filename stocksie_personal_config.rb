46690e6771b925fce197260d9a80c846c6865b25
George Rider <gmrider@gmail.com>
2013-05-01 00:25:51 -0700

    created new classes and refactored

--- stocksie_personal_config.rb
+++ stocksie_personal_config.rb
@@ -4,7 +4,7 @@ require 'stocksie'
 require 'yaml'
 
 module StocksieConfig
-  class Config
+  class Formatting  
     def indent
       "=> "
     end
@@ -16,17 +16,60 @@ module StocksieConfig
     def clear
       system("clear")
     end
+  end
 
+  class Data < Formatting
+    # input needed to evaluate company valuation
+    # could rewrite each string as a class and could assign a method as a get_input method
+    def company_hash
+      company = {
+        :keys => [
+          :common_share, 
+          :preferred_share, 
+          :valuation, 
+          :outstanding_share, 
+          ],
+        :prompts => [
+          "current strike price:  ", 
+          "preferred price per share:  ", 
+          "company valuation/market cap:  ", 
+          "shares outstanding:  "
+          ]
+        }
+    end
+    
+    def grant_hash
+      grant = {
+        :keys => [  
+          :grant_date, 
+          :grant_total, 
+          :strike, 
+          :vesting_period, 
+          :vesting_cliff
+          ],
+        :prompts => [
+          "grant date (MM/DD/YYYY):  ", 
+          "total shares granted:  ", 
+          "strike price:  ", 
+          "vesting term (in months):  ", 
+          "vesting cliff (in months):  "
+          ]
+        }
+    end
+  end
+
+
+  class Config < Data
     # got input validation to work, validates input less than 0 and a data format
-    # re-write at some point to make cleaner.  Possibly break into a class or other methods.
+    # re-write at some point to make cleaner.  Possibly break into a class or other methods.    
     def get_input_and_validate(prompt)
       begin
-      x = STDIN.gets.chomp
+        x = STDIN.gets.chomp
         unless prompt.include? "MM/DD/YYYY"
           x = Float(x)
           raise ArgumentError, "#{indent}Input can't be less than 0 :)" if x < 0
         else 
-          raise ArgumentError, "#{indent}Input should be date format MM/DD/YYYY :)" if x.match(/\d?\d\/\d{2}\/\d{4}/) == nil    
+          raise ArgumentError, "#{indent}Input should be date format MM/DD/YYYY :)" if x.match(/\d?\d\/\d?\d\/\d{4}/) == nil    
           x = x
         end
       rescue ArgumentError => e
@@ -37,34 +80,14 @@ module StocksieConfig
       return x
     end
     
-    # input needed to evaluate company valuation
-    # could rewrite each string as a class and could assign a method as a get_input method
-    # duplicates messages keys to input keys setting ea
-    def user_input_to_hash
-      keys = [:common_share, :preferred_share, :valuation, :outstanding_share, :grant_date, 
-              :grant_total, :strike, :vesting_period, :vesting_cliff]
-
+    def prompt_to_hash(hash = {})
       values = []
-
-      prompts = ["current strike price:  ", 
-                "preferred price per share:  ", 
-                "company valuation/market cap:  ", 
-                "shares outstanding:  ", 
-                "grant date (MM/DD/YYYY):  ", 
-                "total shares granted:  ", 
-                "strike price:  ", 
-                "vesting term (in months):  ", 
-                "vesting cliff (in months):  "]
-
-      prompts.each { |x| print "#{spacing}#{x}"; values << get_input_and_validate(x) }
-      Hash[keys.zip(values)]  
+      hash[:prompts].each { |prompt| print "#{spacing}#{prompt}" ; values << get_input_and_validate(prompt) } 
+      Hash[hash[:keys].zip(values)]
     end
-
-    # rewrote Stocksie classes to accept hashes as input, need to break into two hashes and instantiate.
-    # gonna force it because of respond_to? conditional in class initialize methods.
+    
     def initialize_stocksie
-      arguments = user_input_to_hash
-      OptionsVesting.new(CompanyVal.new(arguments),OptionsGrant.new(arguments))
+      OptionsVesting.new(CompanyVal.new(prompt_to_hash(company_hash)),OptionsGrant.new(prompt_to_hash(grant_hash)))
     end
 
     def save_data(object)
@@ -75,7 +98,7 @@ module StocksieConfig
       success = ["\n", "#{indent}SUCCESS    Data configured", "      File: stocksie_data.yaml created in current directory", "\n"]
       failure = ["\n", "#{indent}ERROR    Config failed", "      File: stocksie_data.yaml not created", "\n"]
       return success.each { |line| puts line } if File.exist?("./stocksie_data.yaml")
-      return failure.each { |line| puts line }
+      failure.each { |line| puts line }
     end
 
     def config
